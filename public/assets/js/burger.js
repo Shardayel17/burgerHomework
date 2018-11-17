3ceec995dee9d9459bb3736eac667e3265902d68
Shardayel17 <Shardayel17@yahoo.com>
2018-11-16 21:51:42 -0500

    much better but not working

---
+++ public/assets/js/burger.js
@@ -0,0 +1,43 @@
+$(function () {
+    $(".btn-EatBurger").on("click", function (event) {
+        var id = $(this).data("id");
+
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: id
+
+        }).then(
+
+            function () {
+                location.reload();
+
+            }
+        );
+
+    });
+
+
+    $("#BurgerNew").on("click", function (event) {
+        event.preventDefault();
+
+
+        var NewBurger = {
+            BurgerName: $("#BurgerNew").val().trim()
+        };
+
+
+        $.ajax("/api/burgers/", {
+            type: "POST",
+            data: NewBurger
+
+
+
+        }).then(
+            function () {
+                location.reload();
+            }
+
+        );
+    });
+});
